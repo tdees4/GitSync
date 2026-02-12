999ff6a6efcad8186638c8740f4cfaa3052278e2
tdees4 <taveon.dees03@gmail.com>
2026-02-12 00:29:43 -0500

    Stylistic changes to /help-webhook

--- src/main/java/io/github/tdees15/gitsync/discord/commands/HelpWebhookCommand.java
+++ src/main/java/io/github/tdees15/gitsync/discord/commands/HelpWebhookCommand.java
@@ -19,13 +19,17 @@ public class HelpWebhookCommand implements SlashCommand {
     @Override
     public void execute(SlashCommandInteractionEvent event) {
         event.reply(
-                ":bulb: SETTING UP A WEBHOOK FOR GITSYNC :bulb:\n\n" +
-                        ":one: Go to your repository and click Settings :right_arrow: Webhooks\n" +
-                        ":two: Click \"Add Webhook\"\n" +
-                        ":three: Copy the url \"https://bot.gitsync-bot.us/webhook/github\" into the text box labeled \"Payload URL\"\n" +
-                        "**OPTIONAL** Enter a randomly generated secret string in the text box labeled \"Secret\"\n" +
-                        ":four: For simple setup, select \"Send me everything\" under \"Which events would you like to trigger this webhook?\"\n" +
-                        ":five: Click \"Add webhook\""
+                        """
+                                :bulb: SETTING UP A WEBHOOK FOR GITSYNC :bulb:
+                                
+                                :one:  Go to your repository and click `Settings` :right_arrow: `Webhooks`
+                                :two:  Click `Add webhook`
+                                :three:  Copy the URL `https://bot.gitsync-bot.us/webhook/github` into the text box labeled `Payload URL`
+                                :four:  **OPTIONAL** Enter a randomly generated secret string in the text box labeled `Secret`
+                                :five:  Ensure SSL Verification is enabled
+                                :six:  For simple setup, select `Send me everything` under *`hich events would you like to trigger this webhook?`
+                                :seven:  Click `Add webhook`
+                                """
                 )
                 .setEphemeral(true)
                 .queue();
